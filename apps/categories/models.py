372dfb4a8f633ca90ec674a07d0274a1160f2c68
vitin <vitor.moraesti@gmail.com>
2025-11-13 16:49:11 -0300

    Refeito adicionado texto de ajuda na descrição

--- apps/categories/models.py
+++ apps/categories/models.py
@@ -2,7 +2,7 @@ from django.db import models
 # Classe para categorizar os itens do leilão
 class Category(models.Model):
     name = models.CharField('Nome', max_length=50)
-    description = models.TextField('Descricao', max_length=100)
+    description = models.TextField('Descricao', max_length=100, help_text="Insira uma descrição detalhada")
     is_active = models.BooleanField('Ativo', default=True)
     registration_date = models.DateField('Data de Cadastro', auto_now_add=True)
 
