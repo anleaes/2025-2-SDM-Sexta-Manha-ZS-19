b72460bb5af96527c0c43688048c2594524e072e
vitin <vitor.moraesti@gmail.com>
2025-11-13 17:13:27 -0300

    Adicionado Documentacao serializer

--- apps/categories/serializer.py
+++ apps/categories/serializer.py
@@ -4,6 +4,7 @@ from .models import Category
 from rest_framework import serializers
 
 class CategorySerializer(serializers.ModelSerializer):
+    # Serializer responsável por converter os objetos de Categoria para JSON
     class Meta:
         model = Category
         fields = '__all__'
