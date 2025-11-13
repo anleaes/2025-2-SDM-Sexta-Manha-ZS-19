6af76fc7c5c70d81fe9f85e1e55df96bdfc280f6
vitin <vitor.moraesti@gmail.com>
2025-11-13 17:38:38 -0300

    Adiciona comentario Default Router

--- apps/categories/urls.py
+++ apps/categories/urls.py
@@ -4,6 +4,7 @@ from rest_framework import routers
 
 app_name = 'categories'
 
+# O DefaultRouter cria automaticamente as rotas para GET, POST, PUT, DELETE
 router = routers.DefaultRouter()
 router.register('', views.CategoryViewSet, basename='categorias')
 
