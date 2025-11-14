635c271a4d9655283abd779d8cf14065ecf53983
vitin <vitor.moraesti@gmail.com>
2025-11-13 22:24:54 -0300

    Adicionando comentario URLs

--- apps/sellers/urls.py
+++ apps/sellers/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
-
+#Rotas da API de Vendedores
 app_name = 'sellers'
 
 router = routers.DefaultRouter()
