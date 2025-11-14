e5d285a7a04c2b6fe6f5807512f4359e94adb8fb
vitin <vitor.moraesti@gmail.com>
2025-11-13 22:54:49 -0300

    Adicionando seller view

--- apps/sellers/views.py
+++ apps/sellers/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from .models import Seller
 from .serializer import SellerSerializer
 
-
+# Create your views here.
 class SellerViewSet(viewsets.ModelViewSet):
-    queryset = Seller.objects.all().order_by('name')
+    queryset = Seller.objects.all()
     serializer_class = SellerSerializer
