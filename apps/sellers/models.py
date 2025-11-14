1cbe12b4dc5fd5820863c60c7dc96f2a8d92f8da
vitin <vitor.moraesti@gmail.com>
2025-11-13 21:47:51 -0300

    docs(seller) comentario seller model

--- apps/sellers/models.py
+++ apps/sellers/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Modelo para os Vendedores
+
 class Seller(models.Model):
-    # Atributos do seu diagrama
+
     name = models.CharField('Nome', max_length=100)
     email = models.EmailField('E-mail', unique=True)
     cpf = models.CharField('CPF', max_length=14, unique=True)
-    # Atributo extra para cumprir a regra dos 4 atributos
     phone = models.CharField('Telefone', max_length=20, blank=True)
 
-    # Meta e str baseados no Roteiro-06
+
     class Meta:
         verbose_name = 'Vendedor'
         verbose_name_plural = 'Vendedores'
