7c175f53adb9fb3943e7ba914233fa8045ac7b00
vitin <vitor.moraesti@gmail.com>
2025-11-18 15:36:06 -0300

    Adicionando comentario no serializer

--- apps/items/serializer.py
+++ apps/items/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Item
-
+# Serializer para os Itens do Leilão
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
