db740b2cf5034ed0d8774c1063c25d5582028012
vitin <vitor.moraesti@gmail.com>
2025-11-18 19:20:21 -0300

    Adicionando BidViewSet

--- apps/bids/views.py
+++ apps/bids/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Bid
+from .serializer import BidSerializer
 
-# Create your views here.
+
+class BidViewSet(viewsets.ModelViewSet):
+    queryset = Bid.objects.all()
+    serializer_class = BidSerializer
