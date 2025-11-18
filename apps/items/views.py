67a76770aa6442018ba108c6b71ec5c405fd67fa
vitin <vitor.moraesti@gmail.com>
2025-11-18 15:52:14 -0300

    Adicionando comentario após o refinamento do query

--- apps/items/views.py
+++ apps/items/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from .models import Item
 from .serializer import ItemSerializer
 
-
+# ViewSet será responsável por gerenciar todas as requisições da API para Itens
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all().order_by('title')
     serializer_class = ItemSerializer
