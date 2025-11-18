ab68fb21448cc9878362ef8ea0ad16f5788619e1
vitin <vitor.moraesti@gmail.com>
2025-11-18 16:40:20 -0300

    Adicionadno a estrutura e review model

---
+++ apps/reviews/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from buyers.models import Buyer
+from sellers.models import Seller
+
+class Review(models.Model):
+    # Foreign Keys
+    buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE, verbose_name='Comprador')
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE, verbose_name='Vendedor Avaliado')
+    
+
+    rating = models.IntegerField('Nota', choices=[(i, i) for i in range(1, 6)]) # 1 a 5 estrelas
+    comment = models.TextField('Comentário', max_length=500)
+    date = models.DateField('Data da Avaliação', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Avaliação'
+        verbose_name_plural = 'Avaliações'
+        # Ordenei da mais recente para a mais antiga
+        ordering =['-date']
+
+    def __str__(self):
+        return f"Avaliação de {self.buyer.name} para {self.seller.name}"
--- leilaoapp/settings.py
+++ leilaoapp/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'reviews.apps.ReviewsConfig',
     'categoryitems.apps.CategoryitemsConfig',
     'items.apps.ItemsConfig',
     'sellers.apps.SellersConfig',
