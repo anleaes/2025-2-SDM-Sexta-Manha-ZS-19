8982327bfe29c5ca1737bc817abdf93248c32706
vitin <vitor.moraesti@gmail.com>
2025-11-18 16:25:46 -0300

    Adicionando comentario pro serializer

--- apps/categoryitems/serializer.py
+++ apps/categoryitems/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import CategoryItem
 
+# Serializer para a Tabela de Ligação M2M
+
 class CategoryItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CategoryItem
