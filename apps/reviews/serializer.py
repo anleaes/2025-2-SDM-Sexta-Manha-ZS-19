2b87aee3d069bf8b09b8947b5fc272a07d830b69
vitin <vitor.moraesti@gmail.com>
2025-11-18 16:49:13 -0300

    Adicionando comentario do serializer

--- apps/reviews/serializer.py
+++ apps/reviews/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Review
 
+# Serializer para as Avaliações de Vendedores
+
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
