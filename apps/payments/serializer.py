5f20d624cd2efa85f769d4f5369c53f2e6f794c7
vitin <vitor.moraesti@gmail.com>
2025-11-20 17:03:28 -0300

    Adicionando comentario pro serializer

--- apps/payments/serializer.py
+++ apps/payments/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Payment
 
+# Serializer para a API de Pagamentos
+
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
